b9076735a68666dafb61e073cb4b700ee25bcede
Mustafa Şükrü Kapusuz <mustafa@intense.com.tr>
2023-04-11 19:46:55 +0300

    wip

--- elementor-widgets-for-tc.php
+++ elementor-widgets-for-tc.php
@@ -1,6 +1,18 @@
+<?php
+
 /*
  * Plugin Name:       Elementor Widgets for Trading Computers
  * Description:       Adds an Elementor Widget for Trading Computers
  * Version:           0.0
  * Text Domain:       elementor-widgets-for-tc
  */
+
+ if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+define('TC_ELEMENTOR_PLUGIN_DIR', plugin_dir_path(__FILE__) );
+
+require_once 'includes/TC_Elementor_Widgets_Init.php';
+
+( new TC_Elementor_Widgets_Init() )->init();
