65cdf39f5e383e9cee0cc3977c9d45fc9b977b35
Mustafa Şükrü Kapusuz <mustafa@intense.com.tr>
2023-04-11 19:36:58 +0300

    chore: init

---
+++ elementor-widgets-for-tc.php
@@ -0,0 +1,6 @@
+/*
+ * Plugin Name:       Elementor Widgets for Trading Computers
+ * Description:       Adds an Elementor Widget for Trading Computers
+ * Version:           0.0
+ * Text Domain:       elementor-widgets-for-tc
+ */
